eb84b8d4a98dd0e72cd7481caeab6070085caf24
Maksym Rybalko <maxymrybalko2005@gmail.com>
2024-04-30 00:00:01 +0300

    Added payments DTO and Response

---
+++ apps/pharmacy-stats-api/dtos/MonthlyPaymentsDTO.ts
@@ -0,0 +1,16 @@
+class MonthlyPaymentsDTO {
+  January: number;
+  February: number;
+  March: number;
+  April: number;
+  May: number;
+  June: number;
+  July: number;
+  August: number;
+  September: number;
+  October: number;
+  November: number;
+  December: number;
+}
+
+export default MonthlyPaymentsDTO;
---
+++ apps/pharmacy-stats-api/responses/MonthlyPaymentsResponse.ts
@@ -0,0 +1,8 @@
+import MonthlyPaymentsDTO from '../dtos/MonthlyPaymentsDTO';
+
+class MonthlyPaymentsResponse {
+  2022: MonthlyPaymentsDTO;
+  2023: MonthlyPaymentsDTO;
+}
+
+export default MonthlyPaymentsResponse;
