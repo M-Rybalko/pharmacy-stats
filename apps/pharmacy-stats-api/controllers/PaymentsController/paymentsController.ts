76008ef46171ec18f313bb7eb008de1dd860d34b
Maksym Rybalko <maxymrybalko2005@gmail.com>
2024-04-30 00:34:23 +0300

    Separated business logic to payments service

---
+++ apps/pharmacy-stats-api/controllers/PaymentsController/paymentsController.ts
@@ -0,0 +1,8 @@
+import paymentsService from '../../services/payments.service';
+
+const getMonthlyPayments = async (req, res) => {
+  const response = paymentsService();
+  res.send(response);
+};
+
+export default getMonthlyPayments;
---
+++ apps/pharmacy-stats-api/services/payments.service.ts
@@ -0,0 +1,20 @@
+import fs from 'fs';
+import MonthlyPaymentsResponse from '../responses/MonthlyPaymentsResponse';
+import paymentParser from '../parsers/payments-parser/PaymentsParser';
+
+const paymentsService = () => {
+  const payments2022 = fs.readFileSync(
+    './data/payments_on_contracts_pharmacy_2022.csv',
+    {encoding: 'utf8'}
+  );
+  const payments2023 = fs.readFileSync(
+    './data/payments_on_contracts_pharmacy_2023.csv',
+    {encoding: 'utf8'}
+  );
+  const response = new MonthlyPaymentsResponse();
+  response['2022'] = paymentParser(payments2022);
+  response['2023'] = paymentParser(payments2023);
+  return response;
+};
+
+export default paymentsService;
